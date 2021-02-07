662cf3af4c34647e8215bd0b16bb62eac143c7fa
Josh Dollinger <32228811+firesidewing@users.noreply.github.com>
2021-02-06 18:25:34 -0800

    more form tessting

--- src/components/RSVP.vue
+++ src/components/RSVP.vue
@@ -7,40 +7,71 @@
       </p>
       <p>Please respond by (yada yada)</p>
     </div>
-    <form name="rsvp" method="POST" data-netlify="true" netlify-honeypot="bot-field">
-      <input
-        type="hidden"
-        name="form-name"
-        value="ask-question"
-      >
+    <form
+      name="rsvp"
+      method="POST"
+      data-netlify="true"
+      netlify-honeypot="bot-field"
+      @submit.prevent="handleSubmit"
+    >
+      <input type="hidden" name="form-name" value="rsvp" />
       <div>
         <label
-          >First Name<input type="text" class="control" name="first-name"
+          >First Name<input
+            type="text"
+            class="control"
+            name="first-name"
+            v-model="form.firstName"
         /></label>
         <label
-          >Last Name<input type="text" class="control" name="last-name"
+          >Last Name<input
+            type="text"
+            class="control"
+            name="last-name"
+            v-model="form.lastName"
         /></label>
         <label>
           How Many in Your Party<input
             type="number"
             class="control"
             name="number"
+            v-model.number="form.numParty"
           />
         </label>
-        <label>Email<input type="email" class="control" name="email" /></label>
+        <label
+          >Email<input
+            type="email"
+            class="control"
+            name="email"
+            v-model="form.email"
+        /></label>
         <label class="attending">
           Attending
           <label>
             Yes
-            <input type="radio" name="attending" value="Attending" />
+            <input
+              type="radio"
+              name="attending"
+              value="Attending"
+              v-model="form.attending"
+            />
           </label>
           <label>
             No
-            <input type="radio" name="attending" value="Not Attending" />
+            <input
+              type="radio"
+              name="attending"
+              value="Not Attending"
+              v-model="form.attending"
+            />
           </label>
         </label>
         <label
-          >Dietary Restrictions<input type="text" class="control" name="diet"
+          >Dietary Restrictions<input
+            type="text"
+            class="control"
+            name="diet"
+            v-model="form.diet"
         /></label>
       </div>
 
@@ -52,7 +83,54 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  name: "RSVP",
+  data() {
+    return {
+      form: {
+        firstName: "",
+        lastName: "",
+        numParty: 0,
+        email: "",
+        attending: "",
+        diet: "",
+      },
+    };
+  },
+  methods: {
+    resetForm() {
+      this.$set(this.form, "firstName", "");
+      this.$set(this.form, "lastName", "");
+      this.$set(this.form, "numParty", 0);
+      this.$set(this.form, "email", "");
+      this.$set(this.form, "attending", "");
+      this.$set(this.form, "diet", "");
+    },
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join("&");
+    },
+    handleSubmit() {
+      const axiosConfig = {
+        header: { "Content-Type": "application/x-www-form-urlencoded" },
+      };
+      axios.post(
+        "/",
+        this.encode({
+          "form-name": "rsvp",
+          ...this.form,
+        }),
+        axiosConfig
+      );
+      this.resetForm();
+    },
+  },
+};
 </script>
 
 <style>
@@ -90,14 +168,14 @@ form div {
   height: 100%;
   align-items: center;
 }
-button[type="submit"]{
-  padding:0.75rem 1rem;
+button[type="submit"] {
+  padding: 0.75rem 1rem;
   margin-top: 1rem;
   background-color: var(--btn);
   color: var(--text);
   border: none;
-  outline:none;
-  color:white;
-  font-size:1.25rem;
+  outline: none;
+  color: white;
+  font-size: 1.25rem;
 }
 </style>
