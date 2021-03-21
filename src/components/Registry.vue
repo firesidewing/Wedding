c4324e8d0dbb75f7dd67147645b15350d938b9ce
Josh Dollinger <32228811+firesidewing@users.noreply.github.com>
2021-03-20 18:17:12 -0700

    adding registry

--- src/components/Registry.vue
+++ src/components/Registry.vue
@@ -1,13 +1,23 @@
 <template>
-  <div></div>
+  <div id='registry'>
+    
+  </div>
 </template>
+
+
 
 <script>
 export default {
-
+  name: "Registry",
+  mounted() {
+    let registryScript = document.createElement('script')
+    registryScript.setAttribute('src', '//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=mjUC49KQVEVuqxwNjhMPnQ2&v=2')
+    registryScript.setAttribute('id', 'script_myregistry_giftlist_iframe')
+    let r = document.getElementById('registry')
+    r.appendChild(registryScript)
+  }
 }
 </script>
-
 <style>
 
 </style>
