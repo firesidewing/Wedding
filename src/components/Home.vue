c85393566e24a098a2908f3f470a144fcad21965
Josh Dollinger <32228811+firesidewing@users.noreply.github.com>
2021-06-06 11:54:02 -0700

    adding pics

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,21 +2,21 @@
   <main class="hello">
     <section class="top-banner-section">
       <div class="banner-image-div">
-        <picture id="hero" class="banner-image hidden">
-          <source media="(min-width:600px)" srcset="@/assets/Hero-min.jpg">
-          <source media="(max-width:600px)" srcset="@/assets/Hero-min-small.jpg">
-          <img src="@/assets/Hero-min.jpg" >
-        </picture>
+        <img
+          class="banner-image"
+          src="https://by3301files.storage.live.com/y4mF_gOemqOGEmU09o30RFy8YWV-tb7Rl0mis7zOw7JhJ6DODilXpvxVKFs6oYwI-6tBnLKw5YugZWl3DBfir0q7b7v8xq7YfRb3MknjwBEFYfXlsjV5-Egr1WU8YMCvx5w-qefQ4Hu39oFcFUbC2SaU9_aV_7fsWdnT89DnIb3E34jJkX6-Lzr7tvNbPklRZaC?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
       </div>
 
       <div id="over" class="banner-overlay-div"></div>
 
       <div class="banner-text-div">
-        <span class="diff">Josh & Tess</span> <br/>
-        <span class="small p1">are getting married!</span> <br/>
-        <span class="smaller">May 23, 2021</span>         
+        <span class="diff">Josh & Tess</span> <br />
+        <span class="small p1">are married!</span> <br />
+        <span class="smaller">May 23, 2021</span>
       </div>
-      
     </section>
   </main>
 </template>
@@ -24,27 +24,24 @@
 <script>
 export default {
   name: "Home",
-  mounted() {
-    let img = document.getElementById('hero')
-    setTimeout(() => img.classList.remove('hidden'), 100)
-  }
+  mounted() {},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.diff{
-  font-family: 'Poiret One', cursive;
+.diff {
+  font-family: "Poiret One", cursive;
   font-size: 3rem;
 }
-.small{
-  font-size:1.5rem;
+.small {
+  font-size: 1.5rem;
 }
 .smaller {
   font-size: 1rem;
 }
-.p1{
-  font-family: 'Poiret One', cursive;
+.p1 {
+  font-family: "Poiret One", cursive;
 }
 .top-banner-section {
   display: grid;
@@ -68,8 +65,8 @@ export default {
 .banner-text-div {
   grid-area: 1 / 1 / 2 / 2;
   margin: auto;
-  font-size:2rem;
-  font-weight:bolder;
+  font-size: 2rem;
+  font-weight: bolder;
   color: #b6b6b6;
   text-align: center;
   z-index: 3;
@@ -77,13 +74,8 @@ export default {
 
 .banner-image {
   display: grid;
-  height:100%;
-  
-  transition: opacity 2s ease-in;
-}
-.banner-image img{
   object-fit: cover;
-  object-position: right;
+  object-position: center;
   min-width: 350px;
   width: 100%;
   height: 100%;
@@ -100,20 +92,17 @@ export default {
   display: grid;
   max-width: 100%;
   background: black;
-  background: rgba(0, 0, 0, 0.56);
+  background: rgba(0, 0, 0, 0.3);
 }
 
-
-
 @media only screen and (max-width: 600px) {
-  .top-banner-section{
-    height:87vh;
+  .top-banner-section {
+    height: 87vh;
     margin: 0;
     grid-template-rows: 87vh;
   }
-  main{
-    padding:0;
+  main {
+    padding: 0;
   }
 }
-
 </style>
--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,73 +1,237 @@
 <template>
-    <div>
-         <div class="center">
-      <p>Current BC Guidlines dictate that only 10 people including the bride/groom and marriage commissioner can be present at the ceremony.</p>
-      <p>For our far away friends: contact us before booking travel.</p>
-      <a href="http://www.bccdc.ca/health-info/diseases-conditions/covid-19/event-planning#:~:text=Funerals%20and%20weddings%20may%20proceed,to%20less%20than%2050%20people">
-        Current Covid Restrictions
-      </a>
+  <div class="container">
+    <div class="section">
+      <div class="title">
+        <span>Set up</span>
+      </div>
+
+      <div class="images">
+        <img
+          src="https://by3301files.storage.live.com/y4m-LmsUxMzBjkRKP7GQWKmcitl_x2wEglmhCjD0AOWYwq6vYOPb32ZWlXImUXn6ja-sterORMTcaVi2gOLBFb9ft3ZiPcdwbLT2RaRUZmPTuOCZ3iwRJrPYNkBj3nOZj8U5fqh97-C2hdh4UIc54EHnBUcK9lF-zXiHZ3Mij41KITtWL9OjyHXS3wCbEIRqnl1?width=8256&height=5493&cropmode=none"
+          width="8256"
+          height="5493"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mKaWOFWKDRZZpmyfrUA9ZAtsadXNhEQZTKRS9Erhlb4tQmFlJaGXXSDGwxt70ZSFm4SIzaXV-s2u0OgqG76yaimkcGd0-vyfv2-6rtkg_2xOcVVEkMaBOEQISaMcBuBj_rLmIXPZ3RB80_14GDd-SblX84p8h8Y79XcCMlwettIErMr7cjYf_qwdakIDL_01Y?width=8256&height=5493&cropmode=none"
+          width="8256"
+          height="5493"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mY9uyCzvEiS-J3HPWSVfl4tTHKBhD7GUiXxEk_CzwgEU4qvoVfTKUGreZTeqyakANUx39gEMn9XeUQLOFsSkCkkrNUY__1KgOLZ9nHz0Ud5Q-NduWfojERXp73LJkbGpvphzIxeM_EiT60tMyuuVtWvc4cR9cdAxJM869puOMau4yLCcH84JtCf1XpPe-WGSN?width=8256&height=5493&cropmode=none"
+          width="8256"
+          height="5493"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mNwfZnUO7qUcgE0EqD-9X5eqFQnlKL3_uA6Zh43vsiaOojUkpA2-Tj_uvJA9s_B_ExoIccXZ-znvrY4YD-oEKSzYR9EDMOq51iu05VpdDjh7nrt9fsGpjB2IU7JG6xQWP0VUyUFDWsTTI7A2EBSCOJ6ffkzYJiDMGQ_C-3830C2Wdtoo8sxiVrtcMsIO0rm62?width=5493&height=8256&cropmode=none"
+          width="5493"
+          height="8256"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mf39voUB4FndZDaIyXic3ftYqzNLDODQyCrN-dBkhrYMmm3iBKupHeg_86ccwwZJbi56NhcCTzaQ3yopU3CFEsZrT0er_w7l2MimARMYrWzCiuaGFYDqMxCrStPtNShNyYPHlTzBFfNCACh5wudhoQ-KhMWHoVbHAsctUw1-EVC7XAbHpb4yNVjFt70aWGxzf?width=3540&height=5321&cropmode=none"
+          width="3540"
+          height="5321"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4m3Wfi-STCsj-RGdlExYWrlugZ8X-TAf90OTJZPlojQGpGpze6VqnX42hx1o84RTpoPBuFuxTh_QDpEFxms3phUMF2xIIPls9LhI5gXaUO9ZqyPN5YFIp_VqDQTFhhQSHFJl2-Hxz99FBnfWh0QNZpk-TLP3pbW_5M4ruD8JHOCqeRxyMGawONNZ_NUR21-me9?width=3894&height=5852&cropmode=none"
+          width="3894"
+          height="5852"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mLxLXmpmG6AB22Zn-4BcKttQjzftLRFl9D45lZzw-lF1Hex8KhAabrLY2vvdwDfhSqbYhjeiz8ZFzRZdjUUeUOsXI55jBYj7PxFCQ7cpk0BCcLDdcuvL6eUUqnDuAjp4JqEPrSw9eMiau7BwmbNIiaAw9vE7Fvc6Qc2jDLq2UMKvaABrThLmDER0uiUHINsU8?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mRb6r79cGi_PZZkXdUcKeuAYM-GZ9LmmFP5FXLbF-JxPnTKo64xF1TlQbH0rU7Dod7Uv8dzms1xtn-1CchIFLoaIWFKC1YDvm3Mgq5dY2qp7tuIMfaCN4tb_uIJEn2x3ojeoNlUdnktNvJYMSskYiY1B88i3Gvy0jhRdwR09axi5QCNBpwgwr87qolhULFM_J?width=8256&height=5493&cropmode=none"
+          width="8256"
+          height="5493"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mmhpzkZUUlKH8PYqGkit4SKLybSNsks-0G_ihBCVufFJi8CnJtpRAmonJNspmepZ0RHHUwJqa9ANqEtBNi1Kz_URwyAr0_S41W94Z-h4mM3wcRSfOMfEBQyDHl9gj7q4WSJGVLQhTlEl5FnZ8LydA8MFYq1JqYunqqrjYyXiuNquRGvBneCOgkVqZmyzUVDoz?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mWpnuQK75zFUwCUAXx3zfr6C4VOfTi3hcusZW0PECyt8Le5VU5pTCew2p8deARx31cTb0Tm_mRCNGY1tNFk4_vfJheVFAOk0O6NdQZwkOjMhELKtH3l9tnesKMRYsYIPPEu_e0PyNDgO0N0YyMriiq8NzIRX0gJ-LGrlkez07ORRrd1A6TL1-2Rnxo7UVQtAZ?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mtnvxaGsV7GYgXX4okDKD8W4-z1tl8e8SyOZ8w_oItf_Xbs84gPahlz9M9MY_KE24cuVMbIr7-6rtnettEf7-LCpFgf9ZEcEmk4XfqpZyTKzLJcwtpfHZkxnNUhFxK02aseZOc_DBrHbK_XSfzradxFFGjxB6auqxsWUntaaYHznvdgQgFWDGLmZiHNL10LK9?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mnEvw6tB9koZ2R---Tl5G_v6rtgIxUBpYNoZiZQulHyHE6KFC8t0M68HA3Vb58l7ufC34yz8J3tgRQYEyfSuV-S_0ofmY96KjjobTSUqEzTRMgC_hZG08YZ592oORJ9wpMGEYOlDNr5erpaHPEvDqj5QkMklx-aPl-rE7SnkeoUohN0TJMLENyGRbd964YNSi?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+      </div>
     </div>
-        <div class="map-link">
-            <h3 class="p1 mt-15">Location</h3>
-            <iframe v-if="IsMobile" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2526.4474785454763!2d-119.2496007842591!3d50.71163697951221!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537ef45f9d66eb2f%3A0xaf13fca1262f6888!2s3270%2016%20Ave%20NE%2C%20Salmon%20Arm%2C%20BC%20V1E%202Y7!5e0!3m2!1sen!2sca!4v1612750144598!5m2!1sen!2sca" width="300" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-            <iframe v-else src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2526.4474785454763!2d-119.2496007842591!3d50.71163697951221!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537ef45f9d66eb2f%3A0xaf13fca1262f6888!2s3270%2016%20Ave%20NE%2C%20Salmon%20Arm%2C%20BC%20V1E%202Y7!5e0!3m2!1sen!2sca!4v1612750144598!5m2!1sen!2sca" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-        </div>    
-        <div class="info">
-            <p>The ceremony and reception will be held at our home in our backyard. Parking TBD.</p>
-        </div>   
+
+    <div class="section">
+      <div class="title">
+        <span>Ceremony</span>
+      </div>
+
+      <div class="images">
+        <img
+          src="https://by3301files.storage.live.com/y4mk9ACBC0P9btUDC5TQQdPHf7qgPR-LxSjvBqtQlysH6nHhn9Dxrref-xftJ3jvy7cbcId80Lh-v9pAxEl9ST6bSVqD-OtRBTgPy0bkZ3O5HBKR8dRWKJWIJ6L_no67FdAFQ3hYVTp2jATX8hQeJlkp6ddGAcn52joXSER3iL3fUbpUw9BFgzHmJBdsjuDa2mW?width=4024&height=6048&cropmode=none"
+          width="4024"
+          height="6048"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4m7fjtm96jsocApB1qEVKXfUZJdq6B19fTCjz0dQbzKfyxOPJ70GlIacBQeLgCGZ2nrnVhwunOsPXg95ee5tl8hGJa-a1j-oRkJiTFY37VxHdKZ-REfHy_QNJsQGjuJkBVJ8yuDCovxM-K9td-GPttOvsfscp9ZRZryWk8dCgVQ63MGw345XaAlj2qR2pDM02t?width=4024&height=6048&cropmode=none"
+          width="4024"
+          height="6048"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mawZLke60DbaxbMeBnkKMJkkzhq2znzUGpoC2u9m6ZMiICQzA1dldtSvD5AOLUjIr82Cg0WCPSXZEF5sO7mijPSNRnQGbIKi1ut6czvApOAq9RzDe9HYNjc_hTu-xzDK6Pal9XAIfqavHmWaPqCFZzmavMFs_LtqiY0_QSLF4sEklkjfubLMZ48W9phvucj31?width=4024&height=6048&cropmode=none"
+          width="4024"
+          height="6048"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mVeHQdoMtZqz8g0HncjngWeq2D0kMkH0YcbrWw9tSH_yt5-SF0HT6ddJBsmUWsnC9S1g81yJrzS5nGPL0kZjVRwZ4LiNV0iSMUouu33jjaDlOguAJT23ScI4o6mRgnCoE4pwGuOTW9pcGYzEOurqo7Db_n94B8OD-wN1iZurbhAWypcOrg83YTrbG0YGO2BMz?width=5238&height=3485&cropmode=none"
+          width="5238"
+          height="3485"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mVdmt8Zlx9ctQKP0adtAFoPrIc1ORZooUCCkPnnLYHGWTnslP6CZIex-GR-NX3nQG7wgzUVzpoDCUykoQiBo8XGOdteNPczB1PeHJKUPmBZa-uL6L6vq_RMtMlLhvxbdNFEBtsmZP3DkDRUYp69Hg0SmTxCpre2Hx0-nHx9fKJ0Dpj2x3i8b7Q1gpfhQrKS6s?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mAbEqJVKOG8RfNySpG6YMLJPQzQhq0YPXGSnAnnf_vMGPigItHrQY9zEyY8Oe4bq5pYTVJZdi7i43qkp-grM8rHES5QxYtf2IyZy28l_9Ey4AlPeE38jsIBfPWFIfDJ5pQdIW1bbA-ovMh1TKXigFD4xwAexkinXRu0PDvMw0EaJiCJraFXGM8Qk1OTYr7KUc?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mRdYgsODVSkcLhXhY6B2LUvWr3rlGGl9uuVFVedzOOJdvE9PybrpAnJhGPwPATTXRRzOwZdBbwf4UhaPDJBJ5i6YvFwnD-Td3D6lJqeZwIrSLSHvivKRIxngGI9hAJCGLbYwlkHwqRC7cgwA5kJtOZ_yCNdnOxjueNhWpYMcyTZtWcihrK-XJYHwHx8K-tnau?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mG0SkS42LnUdEuconiavqgpM24kdiGILRCFAuZixrZUm-YsSajgireY8vTEqReZdSSj2sPKbjYWXFn6HvEsKw30s5iJsZzfLQ4P1VsI7KzGVzuYTETN0l5nAPqCXsFMEKWF9-npFPYQUi2D95V7tI5CaNWAU_OkyDbvvqCK2_KG2PEk2gAFrcsO_JtU-ePuH_?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mcKI_Exk2SYKbfWsy7eoV3bC3LF4VK65HYfRhX1rIOg6lBqTYMDABkEn-EazOCuZ20fgMMDCFU32gwfsdVv0yp_qireOEMb3G4IBDR9o_Jj_-y7sYdnXW-95m33EoZwQ4QD_i2WVvZ1BhKx9oIgLU4dOGSSah3hp4xQuNEKXsuHT2-5ZiPR8-tfvwFACTuxH7?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mKFlezl9APSi3LMzbApGYvzL_wmy54s8du9zHPOrw_dB6RBdA8zZQA36_6Xth5ODcxT3iNJG0spTMlQJ4g0ZxZcz5ZDLsyC9AbKrmnP8ERp_q3igUwrtpTTqeYyuabDqhToeuk2YmMtqKDzzCuddykradYRsGFmKMnCaWzpOvho03uAxodybLdsMncZ8OAowN?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mImZgR9-55JXyhw1O7y7yM9RQOGeTOALwX0OpzmlhmYnwnAfRkkF_cIl6pbXpzsH3Y3cb5c9L1A9ZOFKfNVtFoVUZmnLtMioFakRJzsQM4_Cm8YLj0xYG4jk1iCUcmaReC5LvcTqutyohl8VhbubvFkhPDioVFphJxx9qlTwjl060UsF-75Ltlg9__b7DEPNt?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mxFoJXPveFeAAuDKHcAjZ2isycJIVjrGXwyii2kRwu0DXugcRBs05MRMmNowPY5WLAHjA3zlJdaD8SuazpeATQW8F2nyzdybGsu8TJsKcfV5pojNJRDvKLqKtrPRiiVNnopu4R7daeWIw0TgVdnZbzQAU2SO4kuKByUs5ZVUISIWqXFvS-SKZ7AIQ4CprpFny?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4m9mjBK2GedhG7ay28DsIVq6nLmzd7m54PiAZf28bwhhkqg2S-irfkg_l05Q73rQygQgzJuzug9zMHfh44YYwR94zeqVX0L7-1PYXWb1GljLWug5LTcCv7RCZsvzf46syfGKviIttJGdXRZBLX2tPfjYKYWt2GfYoEmmF4LGacF0b18crMknnKMPhDRpL7rw2h?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mlI5LkpSaQAVzhQ460fg5SsYgDjc0mUe-dEXx1-od4xU9xgLkdt-9g_RDPUeiNRTSfkMhaKQAi3iEpIlQ8-nho77SJBfYGehTQUS07UYueNHO5Sq9GYXebUo1YAPTnu4TxzBx6nhhTzhndstJL7wJeSAhq_w9X-ebmUC7LYt-2j5Vds_LVyqfSfvYotdbVGSm?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mPDqv_I7xrjqvLAYkKY886BnVZ13XBde_RPAf9uB8r2Gg3oh__WS5CbBrnH0rolnCPgWdgBS1Fxg7bfidwR9thW4zBBYcp0EQr4seqKdz0GoEwVDQ0UQHGaJZmNulCt2CvOU5smSMF6XNj_9lQahHRAeeVlgvr4ozR-rGbxUm_LyoIfnFdEXDmkssKmKt1tZi?width=6048&height=4024&cropmode=none"
+          width="6048"
+          height="4024"
+        />
+
+        <img
+          src="https://by3301files.storage.live.com/y4mBIab-8XOxNqkvqvDvrQqZ1TLCNvmHU3K5B-TeAlRrO1XuepnATYquaU64dGKQAK0LUefI1fGoMh-doTHtt2A8pPk54Hgh1paYDcaR3IqcPXhUyuBQy50gh5SFveuhNEAGTzVx44yw6An81l4VvSUF5AZoBmh51jSocc2HAHqrfInEauEudB8SI4evdyAYUB1?width=3294&height=4951&cropmode=none"
+          width="3294"
+          height="4951"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mW2RfK5n579HtUtoR60aUTUX1qcdJAIBQv4wDAGOxsGgi302FVD8LdNmFFUBIJ3XkFz0GmUOerz0FW0yBRYtF7Ny5SdcHgW2Ep68QADRQAPMLZoj0tLOrsb5eC0WY1qjWCEvigAywqJo7IzGsg81PsxKYzA2Nk5sO4LZ1nE-OJbxhlL1cwJnkm9FmXLd5IK0A?width=4024&height=6048&cropmode=none"
+          width="4024"
+          height="6048"
+        />
+        <img
+          src="https://by3301files.storage.live.com/y4mT0iFRaH-C9grC9RZV5vbquVcuGrng600LQEI_gLuaj1Ox4D0JhijJAnKgpkG-0Op2CWx22WjEc-DMGp8QYkioRlap_cBZ4v7vZHpHnttNX_Xc3OseH9r8vheoMCybMKdVrkp8PEJtfP6MJtPPQPzLFFX0K5xQekxAL01Lg93ecRItZc-LxHEYIRyRhI29KXr?width=4024&height=6048&cropmode=none"
+          width="4024"
+          height="6048"
+        />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'info',
-    computed: {
-        IsMobile: function() {
-            return window.innerWidth < 600;
-        }
-    }
-}
+  name: "info",
+  computed: {
+    IsMobile: function () {
+      return window.innerWidth < 600;
+    },
+  },
+};
 </script>
 
 <style>
-.mt-15{
-    margin-top:5rem;
-}
-.p1{
-  font-family: 'Poiret One', cursive;
-}
-.info{
-    text-align: center;
-}
-.map-link {
-  display:grid;
-  justify-content: center;
-  align-items: center;
-  font-size:1.5rem;
-  text-align: center;
+.container {
+  display: grid;
+  grid-gap: 3rem;
+  grid-template-rows: auto;
+  padding: 1rem;
 }
 
-.gg-pin {
-    box-sizing: border-box;
-    position: relative;
-    display: block;
-    transform: rotate(45deg) scale(var(--ggs,1));
-    width: 18px;
-    height: 18px;
-    border-radius: 100% 100% 0 100%;
-    border: 2px solid;
-    margin-top: -4px
+.section {
 }
-.gg-pin::before {
-    content: "";
-    display: block;
-    box-sizing: border-box;
-    position: absolute;
-    width: 8px;
-    height: 8px;
-    border: 2px solid;
-    top: 3px;
-    left: 3px;
-    border-radius: 40px
+
+.images {
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(600px, 1fr));
+}
+
+.images img {
+  height: auto;
+  width: 100%;
+}
+
+.title {
+  display: flex;
+  margin-bottom: 2rem;
+}
+.title span {
+  font-size: 2rem;
+  font-weight: bold;
+  margin: auto;
+  font-family: "Poiret One", cursive;
+}
+
+@media only screen and (max-width: 600px) {
+  .images {
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  }
+  .images img {
+    width: 100%;
+    height: auto;
+  }
 }
 </style>
